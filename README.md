dbdd1c3f8f2350c670708dfbf781af1ad5a21cd2
YùToutCourt <wapzif@gmail.com>
2024-04-26 09:12:57 +0200

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # RustyBeam
 
+Objectifs:
 
-Link : https://web.stanford.edu/class/cs110l/assignments/project-2-2022/
+- Avoir une serveur TCP qui va permettre de recevoir les requêtes des clients et de les rediriger vers le bon serveur.
+
+- Non rupture de serive entre deux site.
+
+- Utilisation de la librairie `tokio` pour la gestion des tâches asynchrones.
+
+- Test d'intégration avec des sites web (Deux site qui seront redirigé par le serveur.)
+
+- Gargo fuzz, QuickCheck,
+
+
+
+Link : 
+
+- https://web.stanford.edu/class/cs110l/assignments/project-2-2022/
+
+- https://rust-fuzz.github.io/book/cargo-fuzz.html
+
+- https://github.com/BurntSushi/quickcheck
--- rustybeam/src/main.rs
+++ rustybeam/src/main.rs
@@ -1,3 +1,52 @@
+use std::env;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::thread;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    while match stream.read(&mut buffer) {
+        Ok(size) => {
+            if size > 0 {
+                println!("Received: {}", String::from_utf8_lossy(&buffer[..size]));
+                stream.write_all(&buffer[..size]).unwrap();
+            }
+            true
+        },
+        Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(std::net::Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Usage: cargo run <IP> <PORT>");
+        std::process::exit(1);
+    }
+
+    let ip = &args[1];
+    let port = &args[2];
+
+    let address = format!("{}:{}", ip, port);
+
+    let listener = TcpListener::bind(address).expect("Could not bind");
+    println!("Server listening on {}:{}", ip, port);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
 }
